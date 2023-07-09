b05a76f8b4a3c023f515bc1436214ff72252a9fc
Pythonidaer <pythonidaer@gmail.com>
2023-07-09 12:31:11 -0400

    added portfolio project cards

--- app/(components)/PortfolioCard.js
+++ app/(components)/PortfolioCard.js
@@ -10,18 +10,22 @@ import data from '../(data)/projects.json'
 
 const PortfolioCard = () => {
   return (
-    <div className='w-11/12 pt-1 md:w-5/6 mx-auto'>
+    <div className='w-11/12 pt-1 md:w-5/6 mx-auto flex flex-wrap justify-start'>
       {data.map((item, index) => (
         <Link
           key={index}
           href={item.url}
           target='_blank'
-          className='mt-4 mx-auto block md:w-96'
+          className='sm:mx-auto md:mx-0 mb-6 md:w-1/2 lg:w-1/3 xl:w-1/3 relative'
         >
-          <div className='relative inline-block rounded-lg w-full'>
-            <img src={item.photo} alt={item.title} className='rounded-lg' />
+          <div className='relative inline-block rounded-lg overflow-hidden group'>
+            <img
+              src={item.photo}
+              alt={item.title}
+              className='rounded-lg filter brightness-100 group-hover:brightness-50 transition duration-300'
+            />
 
-            <div className='absolute bottom-0 left-0 w-full bg-black bg-opacity-70 p-4 text-white rounded-b-lg'>
+            <div className='absolute bottom-0 left-0 right-0 max-w-full w-full bg-black bg-opacity-70 p-4 text-white rounded-b-lg'>
               <h2 className='text-lg font-semibold pb-2'>{item.title}</h2>
               <p className='text-md'>{item.description}</p>
             </div>
@@ -44,22 +48,43 @@ objectPosition='center'
 className='rounded-lg w-full'
 />
 
-<img src='your-image.jpg' alt='Image' class='rounded-lg' />
-    <Link
-      href='https://reddie.herokuapp.com/'
-      target='_blank'
-      className='w-11/12'
-    >
-      <div className=' mx-auto relative inline-block rounded-lg'>
-        <Image
-          src={reddie}
-          alt='3D Render of Michaelangelos David'
-          className=' rounded-lg'
-        />
-        <div className='absolute bottom-0 left-0 w-full bg-black bg-opacity-70 p-4 text-white rounded-b-lg'>
-          <h2 className='text-lg font-semibold pb-2'>Reddie</h2>
-          <p className='text-md'>Custom UI for Saving Reddit Comments</p>
-        </div>
-      </div>
-    </Link>
+    <div className='w-11/12 pt-1 md:w-5/6 mx-auto flex flex-wrap justify-start'>
+      {data.map((item, index) => (
+        <Link
+          key={index}
+          href={item.url}
+          target='_blank'
+          className='sm:sm:mx-auto md:mx-0 mb-6 md:w-1/2 lg:w-1/3 xl:w-1/3'
+        >
+          <div className='relative inline-block rounded-lg '>
+            <img src={item.photo} alt={item.title} className='rounded-lg' />
+
+            <div className='absolute bottom-0 left-0 right-0 max-w-full w-full bg-black bg-opacity-70 p-4 text-white rounded-b-lg'>
+              <h2 className='text-lg font-semibold pb-2'>{item.title}</h2>
+              <p className='text-md'>{item.description}</p>
+            </div>
+          </div>
+        </Link>
+      ))}
+    </div>
+
+          <Link
+          key={index}
+          href={item.url}
+          target='_blank'
+          className='sm:mx-auto md:mx-0 mb-6 md:w-1/2 lg:w-1/3 xl:w-1/3'
+        >
+          <div className='relative inline-block rounded-lg overflow-hidden group'>
+            <img
+              src={item.photo}
+              alt={item.title}
+              className='rounded-lg transform group-hover:scale-105 transition-transform duration-500'
+            />
+
+            <div className='absolute bottom-0 left-0 right-0 max-w-full w-full bg-black bg-opacity-70 p-4 text-white rounded-b-lg '>
+              <h2 className='text-lg font-semibold pb-2'>{item.title}</h2>
+              <p className='text-md'>{item.description}</p>
+            </div>
+          </div>
+        </Link>
     */
--- app/(components)/SectionTwo.js
+++ app/(components)/SectionTwo.js
@@ -4,8 +4,11 @@ const SectionTwo = () => {
   return (
     <section
       id='page2'
-      className='min-h-full bg-gradient-to-b from-blue-100 to-gray-500 relative z-0'
+      className='min-h-full bg-gradient-to-b from-black to-gray-500 relative z-0'
     >
+      <h1 className='text-center text-6xl md:text-9xl md:text-center uppercase py-6 md:py-10 mb-0 font-mono'>
+        Portfolio
+      </h1>
       <PortfolioCard />
     </section>
   )
