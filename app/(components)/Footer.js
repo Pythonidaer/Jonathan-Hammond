ebb62931a70ccf848618b348d709f795f54e0a26
Pythonidaer <pythonidaer@gmail.com>
2023-07-12 15:30:26 -0400

    inverted footer colors just to further emphasize the end of the web app

--- app/(components)/Footer.js
+++ app/(components)/Footer.js
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer
-      className='absolute bottom-0 w-full md:h-10 h-20 bg-white text-center text-black font-bold flex items-center justify-center z-0 px-2 border-t-4 border-accent'
+      className='absolute bottom-0 w-full md:h-10 h-20 bg-black text-center text-gray-200 font-bold flex items-center justify-center z-0 px-2 border-t-4 border-accent'
       id='page5'
     >
       THANKS FOR VISITING!
